1b58f31c66612200bf5f4463ec676c5db730cf79
PanggalihBayuRaka <panggalihbayuraka564@gmail.com>
2024-12-15 17:22:14 +0700

    14des2024

---
+++ JavaScript.js
@@ -0,0 +1,28 @@
+//ini adalah side bar 
+function side() {
+    var text = document.getElementById("SideBar");
+    if (text.style.opacity === "0" || text.style.opacity === "") {
+        text.style.opacity = "1";
+        text.style.visibility = "visible";
+        text.style.transition = "opacity 0.5s ease-in-out, visibility 0.5s ease-in-out";
+    } else {
+        text.style.opacity = "0";
+        text.style.visibility = "hidden";
+        text.style.transition = "opacity 0.5s ease-in-out, visibility 0.5s ease-in-out";
+    }
+}
+//ini adalah lib h1 yakkk
+   document.addEventListener("DOMContentLoaded", function () {
+          new TypeIt("#Kelas", {
+            strings: ["kelas 10 pplg 2"],
+          }).go();
+        });
+        document.addEventListener("DOMContentLoaded", function () {
+            new TypeIt("#Sejarahnya", {
+                strings: "Kami adalah siswa kelas 10 PPLG 2 dari smk yayasan leo sutrisno. Kami memiliki jumlah murid 38 siswa, 33 siswa dan 5 siswi. tidak terlalu banyak informasi tentang kami, karena kami masih dalam proses pembelajaran. terimkasih",
+                speed: 50,
+                waitUntilVisible: true,
+            }).go(); 
+        })
+
+        
